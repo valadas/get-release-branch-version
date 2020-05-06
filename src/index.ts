e6247788830605d78e835f2b8a1cd07c4c72cf11
Daniel Valadas <info@danielvaladas.com>
2020-05-06 02:40:37 -0400

    Commit node_modules

---
+++ src/index.ts
@@ -0,0 +1,14 @@
+import * as core from '@actions/core';
+import * as github from '@actions/github';
+
+async function run() {
+    try {
+        
+    } catch (error) {
+        core.setFailed(error);
+    }
+}
+
+run();
+
+export default run;
